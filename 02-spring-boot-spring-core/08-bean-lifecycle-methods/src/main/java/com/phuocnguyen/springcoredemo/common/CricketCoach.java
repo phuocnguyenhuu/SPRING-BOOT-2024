5f0f3482f04ff56c65740a02d831b2894e6888ca
phuocnguyenhuu <nhphuocworld@gmail.com>
2023-12-28 15:44:16 +0700

    08-bean-lifecycle-methods END

--- 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/phuocnguyen/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/phuocnguyen/springcoredemo/common/CricketCoach.java
@@ -1,16 +1,26 @@
 package com.phuocnguyen.springcoredemo.common;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+
 public class CricketCoach implements Coach{
     public CricketCoach(){
         System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    // define our init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
     }
     @Override
     public String getDailyWorkout() {
